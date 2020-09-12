fa694a4788f9cfcd7f74cb596b9e7ec0979e74f6
Tanvi Ghanpathi <70434776+Tanvi14sept@users.noreply.github.com>
2020-09-12 10:04:17 +0530

    voting

---
+++ voting.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+int main()
+{
+    int limit,i,j,parties,vote,cnt[100],flag[100];
+    printf("How many parties?");
+    scanf("%d",&parties);
+    printf("How many votes allowed on one EVM?");
+    scanf("%d",&limit);
+    for(i=0;i<=parties;i++)
+    {
+        cnt[i]=flag[i]=0;
+    }
+    i=1;
+    do
+    {
+        printf("Enter votes :");
+        scanf("%d",&vote);
+        if(i%(parties+1)!=0)
+            cnt[vote]++;
+        else
+            flag[vote]++;
+        i++;
+    }while(i<=limit);
+    for(i=1;i<=parties;i++)
+    {
+        party[i]=cnt[i]+flag[i];
+        printf("Actual result:",party[i]);
+    }
+    for(i=1;i<=parties-1;i++)
+    {
+        party[i]=cnt[i];
+    }
+    j=0;
+    for(i=1;i<=parties;i++)
+    {
+        j=j+flag[i];
+        party[parties]=cnt[parties]+j;
+        printf("manipulated result:",party[parties]);
+    }
+    return 0;
+}
