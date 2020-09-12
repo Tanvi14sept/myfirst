8e144146ff64c0d3bd5884b12409773b779f98e5
Tanvi Ghanpathi <70434776+Tanvi14sept@users.noreply.github.com>
2020-09-12 09:58:01 +0530

    equal to no. given

---
+++ pairsequaltosum.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main(){
+    int n,cases,a[10],sum,i,j;
+    printf("How many cases:");
+    scanf("%d",&cases);
+    while(cases--)
+    {
+        printf("\n Array size:");
+        scanf("%d",&n);
+        printf("\n Array elements:");
+        for(i=0;i<n;i++)
+        {
+            scanf("%d",&a[i]);
+        printf("\n Desired sum:");
+        scanf("%d",&sum);
+        for(i=0;i<n;i++)
+        {
+            for(j=i+1;j<n-1;j++)
+            {
+                if(a[i]+a[j]==sum)
+                {
+                     printf("(%d %d)\n",a[i],a[j]);
+                }
+            }
+        }
+    }
+}
